80d8e379f71163d80ebd5cded0ff8a5a57a3758c
Devansh Bhatt <devanshbhatt@Devanshs-MacBook-Air.local>
2024-09-17 00:28:11 +0530

    tRPC server setup

---
+++ apps/icloud/src/app/server/index.ts
@@ -0,0 +1,8 @@
+import { router } from "./trpc";
+import { createHTTPServer } from "@trpc/server/adapters/standalone";
+
+const appRouter = router({});
+
+const server = createHTTPServer({ router: appRouter });
+server.listen(3000);
+export type AppRouter = typeof appRouter;
