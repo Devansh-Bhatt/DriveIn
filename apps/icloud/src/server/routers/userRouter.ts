c310bec1906d714380f7bb91b15faa76125f2a98
Devansh Bhatt <devanshbhatt@Devanshs-MacBook-Air.local>
2024-09-22 04:58:32 +0530

    Changes for Deploy 3

--- apps/icloud/src/server/routers/userRouter.ts
+++ apps/icloud/src/server/routers/userRouter.ts
@@ -1,8 +1,5 @@
 import { publicProcedure, router } from "../trpc";
 import { db } from "@/db";
-import { users } from "@/db/schema";
-import { z } from "zod";
-
 export const userRouter = router({
   getUser: publicProcedure
     .meta({ openapi: { method: "GET", path: "/user" } })
@@ -13,15 +10,5 @@ export const userRouter = router({
       } catch (err) {
         console.log("Error : ", err);
       }
-    }),
-  addUser: publicProcedure
-    .meta({ openapi: { method: "POST", path: "/user" } })
-    .input(z.object({ fullname: z.string(), phone: z.string() }))
-    .mutation(async (opts) => {
-      const { input } = opts;
-      await db.insert(users).values({
-        fullName: input.fullname,
-        phone: input.phone,
-      });
     }),
 });
